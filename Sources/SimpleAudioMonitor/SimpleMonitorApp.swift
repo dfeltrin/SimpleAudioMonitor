c42d1d7405addb6a9dd27a0abbd575310a39ee26
Diego Feltrin <5147733+dfeltrin@users.noreply.github.com>
2026-08-29 21:09:41 +0200

    Prevent multiple app instances

--- Sources/SimpleAudioMonitor/SimpleMonitorApp.swift
+++ Sources/SimpleAudioMonitor/SimpleMonitorApp.swift
@@ -1,8 +1,23 @@
+import AppKit
 import SwiftUI
 
 @main
 struct SimpleAudioMonitorApp: App {
     @StateObject private var audioMonitor = AudioMonitor()
+
+    init() {
+        guard let bundleIdentifier = Bundle.main.bundleIdentifier else { return }
+        let currentProcessID = ProcessInfo.processInfo.processIdentifier
+        let existingInstance = NSWorkspace.shared.runningApplications.first {
+            $0.bundleIdentifier == bundleIdentifier && $0.processIdentifier != currentProcessID
+        }
+
+        guard let existingInstance else { return }
+        existingInstance.activate(options: [.activateAllWindows])
+        DispatchQueue.main.async {
+            NSApp.terminate(nil)
+        }
+    }
 
     var body: some Scene {
         WindowGroup {
